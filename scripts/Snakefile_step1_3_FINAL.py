582b10406ee84504d4c7dfcfa13a60b5a6773435
Nithya Kumar <nkumar@hive-dc-7-10-30.hive.hpc.ucdavis.edu>
2025-10-27 13:09:17 -0700

    Updated Snakefile with streamlined method of calling long_samples

--- scripts/Snakefile_step1_3_FINAL.py
+++ scripts/Snakefile_step1_3_FINAL.py
@@ -95,7 +95,7 @@
 import pandas as pd
 
 # Load configuration file
-configfile: "config/config_lemaylab.yaml"
+configfile: "config/config.yaml"
 
 # Path to sample sheet (tab-separated: sample_name, r1_path, r2_path)
 SAMPLE_SHEET = config["sample_sheet"]
